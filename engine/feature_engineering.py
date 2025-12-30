7bc666656555971351d4dfe4de38a5ae83d1abba
swapnilramdaswaghi21-ux <swapnilramdas.waghi21@iimranchi.ac.in>
2025-12-30 14:18:48 +0530

    Update feature_engineering.py

--- engine/feature_engineering.py
+++ engine/feature_engineering.py
@@ -3,8 +3,8 @@ def build_features(df):
     df["Low_Quality"] = 9 - df["F_Score"]
     df["High_Leverage"] = df["Debt_Equity"]
     df["Weak_Cashflow"] = -df["CFO_Growth"]
-
     return df[
-        ["Hybrid_EM","PEG","Low_Quality","High_Leverage","Weak_Cashflow"]
+        ["Hybrid_EM", "PEG", "Low_Quality", "High_Leverage", "Weak_Cashflow"]
     ]
+
 
