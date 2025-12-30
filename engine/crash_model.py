e3167395e8e6adad83f0adc6c40e98a483e6604c
swapnilramdaswaghi21-ux <swapnilramdas.waghi21@iimranchi.ac.in>
2025-12-30 13:58:20 +0530

    Update crash_model.py

--- engine/crash_model.py
+++ engine/crash_model.py
@@ -1,12 +1,10 @@
 from sklearn.ensemble import GradientBoostingClassifier
 
 def train_model(df, X):
-    df = df.copy()
-    df["Crash"] = (df["Return"] < -0.30).astype(int)
-
+    y = (df["Return"] < -0.30).astype(int)
     model = GradientBoostingClassifier()
-    model.fit(X, df["Crash"])
+    model.fit(X, y)
     return model
 
-def predict_probabilities(model, X):
-    return model.predict_proba(X)[:, 1]
+def predict(model, X):
+    return model.predict_proba(X)[:,1]
